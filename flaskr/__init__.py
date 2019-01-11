851f6a9d4f9d32654317888607a723029d416eac
unknown <dingran@uki.com>
2019-01-11 15:40:48 +0800

    准备测试覆盖

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -40,6 +40,7 @@ def create_app(test_config=None):
     from . import db
     db.init_app(app)
 
+    # 注册蓝图
     from . import auth
     app.register_blueprint(auth.bp)
 
--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, url_for, redirect, abort
+from flask.cli import main
 
 print('begin new Flask __name__ = %s --my' % __name__)
 app = Flask(__name__)
@@ -12,4 +13,7 @@ if __name__ == "__main__":
     '''
     可以通过这个启动debug模式
     '''
+    url_for()
+    redirect()
+    abort()
     app.run(host='127.0.0.1', port=5001)
